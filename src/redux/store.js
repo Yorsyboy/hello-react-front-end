0eb6da3a0385a9526a67dcaaf5ab31d43ff028ff
Toyosi Taiwo <adekunletaiwo43@yahoo.com>
2022-09-13 22:50:02 +0100

    Add redux store

---
+++ src/redux/store.js
@@ -0,0 +1,8 @@
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import { greetingReducer } from './greetings/greetings';
+
+const store = createStore(greetingReducer, applyMiddleware(logger, thunk));
+
+export default store;
